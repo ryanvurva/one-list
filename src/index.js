3c3e2c69c90710df4db3932ca44f3249d7555765
Ryan Vurva <ryanvurva@gmail.com>
2017-03-28 15:48:31 -0400

    everything but the local storage is done.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,31 @@ if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  const input = document.querySelector('input')
+  const list = document.querySelector('ul.one-list')
+  const button = document.querySelector('button')
+  const form = document.querySelector('form')
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault()
+    const listItem = input.value
+    const li = document.createElement('li')
+    li.textContent = listItem
+    list.appendChild(li)
+    let didDblClick = false
+    li.addEventListener('click', () => {
+      setTimeout(() => {
+        if (!didDblClick) {
+          li.style.textDecoration = 'line-through'
+        }
+      }, 300)
+    })
+    li.addEventListener('dblclick', (event) => {
+      didDblClick = true
+      list.removeChild(li)
+    })
+    form.reset()
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main)
